cd71d3341d906be9df9646eb86a0713bfa878d7c
tanyakanf <tanyakanf@gmail.com>
2024-07-06 21:49:46 +0200

    start on user profile page

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { AuthContext } from "@context/authContext";
+import profilePic from "@/assets/pic.jpeg";
 
 function UserPage() {
-  return <main>UserPage</main>;
+  const { user } = useContext(AuthContext);
+
+  const [userData, setUserData] = useState({
+    name: "",
+    profileImg: "",
+  });
+
+  useEffect(() => {
+    if (user) setUserData({ name: user.name, profileImg: user.profileImg });
+  }, []);
+
+  const handleOnChange = (e) => {
+    setUserData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  return (
+    <main className="login">
+      <img src={profilePic} alt="profile picture" width="150" />
+      <label>
+        Name:
+        <input
+          name="name"
+          type="text"
+          value={userData.name}
+          onChange={handleOnChange}
+        />
+      </label>
+      <button disabled={userData.name === user.name}>Save</button>
+    </main>
+  );
 }
 
 export default UserPage;
