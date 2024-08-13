9234f8bb383086cbfd86de48de4b6bc716cb0961
tanyakanf <tanyakanf@gmail.com>
2024-08-13 12:42:14 +0200

    style loadi g page

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { CircularProgress, Box, Typography } from "@mui/material";
 
 function Loading() {
-  return <p>Loading</p>;
+  return (
+    <Box
+      sx={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <CircularProgress size={50} thickness={3} />
+      <Typography sx={{ color: "primary.main", mt: "30px" }}>
+        Verifying user...
+      </Typography>
+    </Box>
+  );
 }
 
 export default Loading;
