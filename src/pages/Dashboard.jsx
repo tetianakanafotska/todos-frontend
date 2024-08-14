f3daff2d0445d6853ce20a60f9e8eaea3e41537e
tanyakanf <tanyakanf@gmail.com>
2024-08-14 17:12:03 +0200

    use task context

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,14 +6,10 @@ import EditTask from "./EditTask";
 import AddTask from "./AddTask";
 import tasksService from "@services/task.service";
 import { DragDropContext } from "react-beautiful-dnd";
+import { useTasks } from "@context/taskContext.jsx";
 
 function Dashboard() {
-  const [toDoTasks, setToDoTasks] = useState([]);
-  const [inProgressTasks, setInProgressTasks] = useState([]);
-  const [doneTasks, setDoneTasks] = useState([]);
-  const [allTasks, setAllTasks] = useState([]);
-
-  const [loading, setLoading] = useState(true);
+  const { tasks, fetchTasks, loading, setTasks } = useTasks();
   const [openAddTask, setOpenAddTask] = useState(false);
   const [openEditTask, setOpenEditTask] = useState(false);
 
@@ -22,40 +18,27 @@ function Dashboard() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const fetchTasksByType = async () => {
-      try {
-        const toDo = await tasksService.getByType("toDo");
-        const inProgress = await tasksService.getByType("inProgress");
-        const done = await tasksService.getByType("done");
-        setToDoTasks(toDo.data);
-        setInProgressTasks(inProgress.data);
-        setDoneTasks(done.data);
-        setLoading(false);
-      } catch (error) {
-        console.error("Failed to fetch and set tasks:", error);
-      }
-    };
-    fetchTasksByType();
-  }, []);
+    fetchTasks();
+  }, [fetchTasks]);
 
   useEffect(() => {
-    setAllTasks([...toDoTasks, ...inProgressTasks, ...doneTasks]);
-  }, [toDoTasks, inProgressTasks, doneTasks]);
+    const allTasks = Object.values(tasks).flat();
+    const taskExists = allTasks.some((task) => task._id === taskId);
 
-  useEffect(() => {
-    if (allTasks.length > 0 && taskId) {
-      const taskExists = allTasks.some((task) => task._id === taskId);
-      if (!taskExists) {
-        navigate("*");
-      } else {
+    if (taskId) {
+      if (taskExists) {
         setOpenEditTask(true);
+      } else {
+        navigate("*");
       }
     }
+  }, [taskId, tasks, navigate, setOpenEditTask]);
 
+  useEffect(() => {
     if (taskType) {
       setOpenAddTask(true);
     }
-  }, [taskId, taskType, allTasks]);
+  }, [taskType]);
 
   const handleDragEnd = async (result) => {
     const { source, destination } = result;
@@ -65,12 +48,7 @@ function Dashboard() {
 
     if (sourceId === destId && sourceIndex === destIndex) return;
 
-    const taskMap = {
-      toDo: allTasks.filter((task) => task.type === "toDo"),
-      inProgress: allTasks.filter((task) => task.type === "inProgress"),
-      done: allTasks.filter((task) => task.type === "done"),
-    };
-
+    const taskMap = { ...tasks };
     const sourceTasks = [...taskMap[sourceId]];
     const destTasks = sourceId === destId ? sourceTasks : [...taskMap[destId]];
     const [movedTask] = sourceTasks.splice(sourceIndex, 1);
@@ -83,16 +61,12 @@ function Dashboard() {
     const updatedTask = { ...movedTask, type: destId, position: newPosition };
     destTasks.splice(destIndex, 0, updatedTask);
 
-    const updateState = (sourceId, destId, sourceTasks, destTasks) => {
-      const setters = {
-        toDo: setToDoTasks,
-        inProgress: setInProgressTasks,
-        done: setDoneTasks,
-      };
-      setters[destId](destTasks);
-      setters[sourceId](sourceTasks);
-    };
-    updateState(sourceId, destId, sourceTasks, destTasks);
+    setTasks((prevTasks) => ({
+      ...prevTasks,
+      [sourceId]: sourceTasks,
+      [destId]: destTasks,
+    }));
+
     await tasksService.put(movedTask._id, updatedTask);
   };
 
@@ -102,16 +76,16 @@ function Dashboard() {
         <EditTask
           open={openEditTask}
           setOpen={setOpenEditTask}
-          allTasks={allTasks}
-          setAllTasks={setAllTasks}
+          tasks={tasks}
+          setTasks={setTasks}
         />
       )}
       {openAddTask && (
         <AddTask
           open={openAddTask}
           setOpen={setOpenAddTask}
-          allTasks={allTasks}
-          setAllTasks={setAllTasks}
+          tasks={tasks}
+          setTasks={setTasks}
         />
       )}
       <DragDropContext onDragEnd={handleDragEnd}>
@@ -132,10 +106,7 @@ function Dashboard() {
                 height={80}
               />
             ) : (
-              <TaskList
-                taskType="toDo"
-                tasks={allTasks.filter((task) => task.type === "toDo")}
-              />
+              <TaskList taskType="toDo" tasks={tasks["toDo"]} />
             )}
           </Grid>
 
@@ -148,10 +119,7 @@ function Dashboard() {
                 height={80}
               />
             ) : (
-              <TaskList
-                taskType="inProgress"
-                tasks={allTasks.filter((task) => task.type === "inProgress")}
-              />
+              <TaskList taskType="inProgress" tasks={tasks["inProgress"]} />
             )}
           </Grid>
 
@@ -164,10 +132,7 @@ function Dashboard() {
                 height={80}
               />
             ) : (
-              <TaskList
-                taskType="done"
-                tasks={allTasks.filter((task) => task.type === "done")}
-              />
+              <TaskList taskType="done" tasks={tasks["done"]} />
             )}
           </Grid>
         </Grid>
@@ -177,148 +142,3 @@ function Dashboard() {
 }
 
 export default Dashboard;
-
-// import React, { useEffect, useState } from "react";
-// import { useNavigate, useParams } from "react-router-dom";
-// import { Grid, Skeleton } from "@mui/material";
-// import TaskList from "@components/TaskList";
-// import EditTask from "./EditTask";
-// import AddTask from "./AddTask";
-// import tasksService from "@services/task.service";
-// import { DragDropContext } from "react-beautiful-dnd";
-// import { useTasks } from "@context/taskContext.jsx";
-
-// function Dashboard() {
-//   const { tasks, fetchTasks, loading, setTasks } = useTasks();
-//   const [openAddTask, setOpenAddTask] = useState(false);
-//   const [openEditTask, setOpenEditTask] = useState(false);
-
-//   const { taskId } = useParams();
-//   const { taskType } = useParams();
-//   const navigate = useNavigate();
-
-//   useEffect(() => {
-//     fetchTasks();
-//   }, [fetchTasks]);
-
-//   useEffect(() => {
-//     const allTasks = Object.values(tasks).flat();
-//     const taskExists = allTasks.some((task) => task._id === taskId);
-
-//     if (taskId) {
-//       if (taskExists) {
-//         setOpenEditTask(true);
-//       } else {
-//         navigate("*");
-//       }
-//     }
-//   }, [taskId, tasks, navigate, setOpenEditTask]);
-
-//   useEffect(() => {
-//     if (taskType) {
-//       setOpenAddTask(true);
-//     }
-//   }, [taskType]);
-
-//   const handleDragEnd = async (result) => {
-//     const { source, destination } = result;
-//     if (!destination) return;
-//     const { index: sourceIndex, droppableId: sourceId } = source;
-//     const { index: destIndex, droppableId: destId } = destination;
-
-//     if (sourceId === destId && sourceIndex === destIndex) return;
-
-//     const taskMap = { ...tasks };
-//     const sourceTasks = [...taskMap[sourceId]];
-//     const destTasks = sourceId === destId ? sourceTasks : [...taskMap[destId]];
-//     const [movedTask] = sourceTasks.splice(sourceIndex, 1);
-//     const taskLeft = destTasks[destIndex - 1];
-//     const taskRight = destTasks[destIndex];
-//     const leftPosition = taskLeft ? taskLeft.position : 0;
-//     const rightPosition = taskRight ? taskRight.position : leftPosition + 2;
-//     const newPosition = (leftPosition + rightPosition) / 2;
-
-//     const updatedTask = { ...movedTask, type: destId, position: newPosition };
-//     destTasks.splice(destIndex, 0, updatedTask);
-
-//     setTasks((prevTasks) => ({
-//       ...prevTasks,
-//       [sourceId]: sourceTasks,
-//       [destId]: destTasks,
-//     }));
-
-//     await tasksService.put(movedTask._id, updatedTask);
-//   };
-
-//   return (
-//     <>
-//       {openEditTask && (
-//         <EditTask
-//           open={openEditTask}
-//           setOpen={setOpenEditTask}
-//           tasks={tasks}
-//           setTasks={setTasks}
-//         />
-//       )}
-//       {openAddTask && (
-//         <AddTask
-//           open={openAddTask}
-//           setOpen={setOpenAddTask}
-//           tasks={tasks}
-//           setTasks={setTasks}
-//         />
-//       )}
-//       <DragDropContext onDragEnd={handleDragEnd}>
-//         <Grid
-//           container
-//           spacing="10px"
-//           component="main"
-//           sx={{ ml: "80px" }}
-//           width="calc(100vw - 80px)"
-//           className="main-container"
-//         >
-//           <Grid item lg={4} md={4} sm={12} xs={12}>
-//             {loading ? (
-//               <Skeleton
-//                 animation="wave"
-//                 variant="rounded"
-//                 width="100%"
-//                 height={80}
-//               />
-//             ) : (
-//               <TaskList taskType="toDo" tasks={tasks["toDo"]} />
-//             )}
-//           </Grid>
-
-//           <Grid item lg={4} md={4} sm={12} xs={12}>
-//             {loading ? (
-//               <Skeleton
-//                 animation="wave"
-//                 variant="rounded"
-//                 width="100%"
-//                 height={80}
-//               />
-//             ) : (
-//               <TaskList taskType="inProgress" tasks={tasks["inProgress"]} />
-//             )}
-//           </Grid>
-
-//           <Grid item lg={4} md={4} sm={12} xs={12}>
-//             {loading ? (
-//               <Skeleton
-//                 animation="wave"
-//                 variant="rounded"
-//                 width="100%"
-//                 height={80}
-//               />
-//             ) : (
-//               <TaskList taskType="done" tasks={tasks["done"]} />
-//             )}
-//           </Grid>
-//         </Grid>
-//       </DragDropContext>
-//     </>
-//   );
-// }
-
-// export default Dashboard;
--- src/pages/EditTask.jsx
+++ src/pages/EditTask.jsx
@@ -1,9 +1,10 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import TaskEditModal from "@components/TaskEditModal";
 import CloseIcon from "@mui/icons-material/Close";
 import tasksService from "@services/task.service";
 import { UserContext } from "@context/userContext";
+import { useTasks } from "@context/taskContext.jsx";
 import {
   IconButton,
   Button,
@@ -12,52 +13,68 @@ import {
   DialogActions,
 } from "@mui/material";
 
-function EditTask({ allTasks, setAllTasks, open, setOpen }) {
+function EditTask({ open, setOpen }) {
   const { user } = useContext(UserContext);
   const navigate = useNavigate();
   const { taskId } = useParams();
+  const { tasks, setTasks, fetchTasks } = useTasks();
 
-  const currentTask = allTasks.find((task) => task._id === taskId);
+  const findTaskById = (taskId) => {
+    const allTasks = [
+      ...(tasks.toDo || []),
+      ...(tasks.inProgress || []),
+      ...(tasks.done || []),
+    ];
+    return allTasks.find((task) => task._id === taskId);
+  };
+
+  const currentTask = findTaskById(taskId);
 
   const [formInputs, setFormInputs] = useState({
-    title: currentTask.title,
-    type: currentTask.type,
-    priority: currentTask.priority,
-    description: currentTask.description,
-    position: currentTask.position,
-    createdAt: currentTask.createdAt,
-    dueAt: currentTask.dueAt,
+    title: "",
+    type: "",
+    priority: "",
+    description: "",
+    createdAt: "",
+    dueAt: "",
   });
 
-  const handleSave = () => {
-    tasksService
-      .put(taskId, { ...formInputs, user: user._id })
-      .then((updatedTask) => {
-        const updatedTasks = allTasks.map((task) =>
-          task._id === taskId ? updatedTask.data : task
-        );
-        setAllTasks(updatedTasks);
-        setOpen(false);
-        navigate("/dashboard");
-      })
-      .catch((err) => {
-        console.error(err);
+  useEffect(() => {
+    if (currentTask) {
+      setFormInputs({
+        title: currentTask.title,
+        type: currentTask.type,
+        priority: currentTask.priority,
+        description: currentTask.description,
+        position: currentTask.position,
+        createdAt: currentTask.createdAt,
+        dueAt: currentTask.dueAt,
       });
+    }
+  }, [currentTask]);
+
+  const handleSave = async () => {
+    try {
+      await tasksService.put(taskId, { ...formInputs, user: user._id });
+      await fetchTasks();
+      setOpen(false);
+      navigate("/dashboard");
+    } catch (err) {
+      console.error(err);
+    }
   };
 
-  const handleDelete = () => {
-    tasksService
-      .delete(taskId)
-      .then(() => {
-        const updatedTasks = allTasks.filter((task) => task._id != taskId);
-        setAllTasks(updatedTasks);
-        setOpen(false);
-        navigate("/dashboard");
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+  const handleDelete = async () => {
+    try {
+      await tasksService.delete(taskId);
+      await fetchTasks();
+      setOpen(false);
+      navigate("/dashboard");
+    } catch (err) {
+      console.error(err);
+    }
   };
+
   const handleClose = () => {
     setOpen(false);
     navigate("/dashboard");
@@ -97,123 +114,3 @@ function EditTask({ allTasks, setAllTasks, open, setOpen }) {
 }
 
 export default EditTask;
-
-// import React, { useContext, useState, useEffect } from "react";
-// import { useNavigate, useParams } from "react-router-dom";
-// import TaskEditModal from "@components/TaskEditModal";
-// import CloseIcon from "@mui/icons-material/Close";
-// import tasksService from "@services/task.service";
-// import { UserContext } from "@context/userContext";
-// import { useTasks } from "@context/taskContext.jsx";
-// import {
-//   IconButton,
-//   Button,
-//   Dialog,
-//   DialogContent,
-//   DialogActions,
-// } from "@mui/material";
-
-// function EditTask({ open, setOpen }) {
-//   const { user } = useContext(UserContext);
-//   const navigate = useNavigate();
-//   const { taskId } = useParams();
-//   const { tasks, setTasks, fetchTasks } = useTasks();
-
-//   const findTaskById = (taskId) => {
-//     const allTasks = [
-//       ...(tasks.toDo || []),
-//       ...(tasks.inProgress || []),
-//       ...(tasks.done || []),
-//     ];
-//     return allTasks.find((task) => task._id === taskId);
-//   };
-
-//   const currentTask = findTaskById(taskId);
-
-//   const [formInputs, setFormInputs] = useState({
-//     title: "",
-//     type: "",
-//     priority: "",
-//     description: "",
-//     createdAt: "",
-//     dueAt: "",
-//   });
-
-//   useEffect(() => {
-//     if (currentTask) {
-//       setFormInputs({
-//         title: currentTask.title,
-//         type: currentTask.type,
-//         priority: currentTask.priority,
-//         description: currentTask.description,
-//         createdAt: currentTask.createdAt,
-//         dueAt: currentTask.dueAt,
-//       });
-//     }
-//   }, [currentTask]);
-
-//   const handleSave = async () => {
-//     try {
-//       await tasksService.put(taskId, { ...formInputs, user: user._id });
-//       //await fetchTasks();
-//       setTasks((prev) => ({
-//         ...prev,
-//         [formInputs.type]: [...prev[formInputs.type], savedTask.data],
-//       }));
-//       setOpen(false);
-//       navigate("/dashboard");
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   };
-
-//   const handleDelete = async () => {
-//     try {
-//       await tasksService.delete(taskId);
-//       await fetchTasks();
-//       setOpen(false);
-//       navigate("/dashboard");
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   };
-
-//   const handleClose = () => {
-//     setOpen(false);
-//     navigate("/dashboard");
-//   };
-
-//   return (
-//     <Dialog open={open} onClose={handleClose} fullWidth>
-//       <IconButton
-//         aria-label="close"
-//         onClick={handleClose}
-//         sx={{
-//           position: "absolute",
-//           right: 8,
-//           top: 8,
-//         }}
-//       >
-//         <CloseIcon />
-//       </IconButton>
-//       <DialogContent sx={{ p: "30px 10px 15px", mt: "20px" }}>
-//         <TaskEditModal formInputs={formInputs} setFormInputs={setFormInputs} />
-//         <DialogActions>
-//           <Button
-//             onClick={handleSave}
-//             variant="contained"
-//             color="black"
-//             disabled={formInputs.title === ""}
-//           >
-//             Save
-//           </Button>
-//           <Button onClick={handleDelete} variant="outlined" color="black">
-//             Delete
-//           </Button>
-//         </DialogActions>
-//       </DialogContent>
-//     </Dialog>
-//   );
-// }
-
-// export default EditTask;
